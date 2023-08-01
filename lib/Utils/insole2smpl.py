cade1b5cfe7f35415be0f1a1a804d298136ad3e7
zhanghebuaa <zhanghebuaa@163.com>
2023-08-01 12:01:02 +0800

    label smpl contact

--- lib/Utils/insole2smpl.py
+++ lib/Utils/insole2smpl.py
@@ -23,13 +23,13 @@ def mapInsole2Smpl():
     for ids in footIdsR:
         tmp = np.stack(np.where(masksmplR == ids))
         lsR[str(ids)] = tmp
-    np.save('essentials/pressure/insole2smplR.npy', lsR)
+    np.save('debug/insole2smplR.npy', lsR)
 
     lsL = {}
     for ids in footIdsL:
         tmp = np.stack(np.where(masksmplL == ids))
         lsL[str(ids)] = tmp
-    np.save('essentials/pressure/insole2smplL.npy', lsL)
+    np.save('debug/insole2smplL.npy', lsL)
     exit()
 
 
@@ -118,68 +118,164 @@ def show_insole(idx,insole_data):
     cv2.waitKey(1)
     return img
 
-def footRegion():
+def footRegionMapping():
     '''
     :return: (row_num,col_num)
     '''
-    insoleRegion={}
+    insoleRegionR={}
     insoleMaskR = np.loadtxt('E:\dataset\PressureDataset\insole_mask/insoleMaskR.txt').astype(np.int32)
     ic(insoleMaskR.shape)
 
     BigToe = np.array(np.where((insoleMaskR[:6,:5]>0.5)))
-    insoleRegion['BigToe'] = BigToe
+    insoleRegionR['BigToe'] = BigToe
 
     MidToe = np.array(np.where((insoleMaskR[:6, 5:8] > 0.5)))
     MidToe[1,:] = MidToe[1,:]+5
-    insoleRegion['MidToe'] = MidToe
+    insoleRegionR['MidToe'] = MidToe
 
     SmallToe = np.array(np.where((insoleMaskR[:6, 8:] > 0.5)))
     SmallToe[1,:] = SmallToe[1,:]+8
-    insoleRegion['SmallToe'] = SmallToe
-
-    SmallToe = np.array(np.where((insoleMaskR[:6, 8:] > 0.5)))
-    SmallToe[1,:] = SmallToe[1,:]+8
-    insoleRegion['SmallToe'] = SmallToe
+    insoleRegionR['SmallToe'] = SmallToe
 
     Metatarsus0 = np.array(np.where((insoleMaskR[6:15, :4] > 0.5)))
     Metatarsus0[0,:] = Metatarsus0[0,:] + 6
-    insoleRegion['Metatarsus0'] = Metatarsus0
+    insoleRegionR['Front0'] = Metatarsus0
 
     Metatarsus1 = np.array(np.where((insoleMaskR[6:15, 4:8] > 0.5)))
     Metatarsus1[0,:] = Metatarsus1[0,:] + 6
     Metatarsus1[1,:] = Metatarsus1[1,:] + 4
-    insoleRegion['Metatarsus1'] = Metatarsus1
+    insoleRegionR['Front1'] = Metatarsus1
 
     Metatarsus2 = np.array(np.where((insoleMaskR[6:15, 8:] > 0.5)))
     Metatarsus2[0,:] = Metatarsus2[0,:] + 6
     Metatarsus2[1,:] = Metatarsus2[1,:] + 8
-    insoleRegion['Metatarsus2'] = Metatarsus2
+    insoleRegionR['Front2'] = Metatarsus2
 
     Cuboid0 = np.array(np.where((insoleMaskR[15:25, :8] > 0.5)))
     Cuboid0[0,:] = Cuboid0[0,:] + 15
-    insoleRegion['Cuboid0'] = Cuboid0
+    insoleRegionR['Mid0'] = Cuboid0
 
     Cuboid1 = np.array(np.where((insoleMaskR[15:25, 8:] > 0.5)))
     Cuboid1[0,:] = Cuboid1[0,:] + 15
     Cuboid1[1,:] = Cuboid1[1,:] + 8
-    insoleRegion['Cuboid1'] = Cuboid0
+    insoleRegionR['Mid1'] = Cuboid1
 
     Calcaneus0 = np.array(np.where((insoleMaskR[25:, :8] > 0.5)))
     Calcaneus0[0,:] = Calcaneus0[0,:] + 25
-    insoleRegion['Calcaneus0'] = Calcaneus0
+    insoleRegionR['Back0'] = Calcaneus0
 
     Calcaneus1 = np.array(np.where((insoleMaskR[25:, 8:] > 0.5)))
     Calcaneus1[0, :] = Calcaneus1[0, :] + 25
     Calcaneus1[1, :] = Calcaneus1[1, :] + 8
-    insoleRegion['Calcaneus1'] = Calcaneus1
+    insoleRegionR['Back1'] = Calcaneus1
+
+    smplRegionR={}
+    smplRegionR['BigToe'] = np.array([6637,6640,6636,6639,6693,6695,6697,6696])
+    smplRegionR['MidToe'] = np.array([6621,6622,6654,6651,6653,6652,6696,6701,6707,6661,6663,6664,6660,6706,6705])
+    smplRegionR['SmallToe'] = np.array([6630,6629,6674,6677,6675,6678,6707,6711,6716,6687,6690,6686,6689,6626,6625])
+    smplRegionR['Front0'] = np.array([6752,6753,6806,6837,6755,6754])
+    smplRegionR['Front1'] = np.array([6758,6759,6838,6839,6761,6760])
+    smplRegionR['Front2'] = np.array([6762,6757,6763,6756,6840,6819])
+    smplRegionR['Mid0'] = np.array([6807,6844,6843,6808,6848,6847,6830,6849,6850])
+    smplRegionR['Mid1'] = np.array([6842,6841,6820,6846,6845,6821,6851,6852,6822])
+    smplRegionR['Back0'] = np.array([6831,6856,6855,6829,6862,6863,6828,6861,6864,6867,6860,6827])
+    smplRegionR['Back1'] = np.array([6854,6853,6823,6865,6857,6824,6866,6859,6826,6868,6858,6825])
+
+    RegionInsole2SMPLR={'insole':insoleRegionR,
+                        'smpl':smplRegionR}
+    np.save('debug/insole2smpl/RegionInsole2SMPLR.npy', RegionInsole2SMPLR)
     exit()
 
-    np.save('debug/insole2smpl/insoleRegion.npy',insoleRegion)
+def footRegionMappingRight2Left():
+    footIdsLR = np.loadtxt('essentials/footLR_ids.txt').astype(np.int32)
+
+    RegionInsole2SMPL = np.load('debug/insole2smpl/RegionInsole2SMPLR.npy', allow_pickle=True).item()
+    insoleRegion = RegionInsole2SMPL['insole']
+    smplRegion = RegionInsole2SMPL['smpl']
+
+    all_idsR = []
+    for _key in smplRegion.keys():
+        all_idsR.append(smplRegion[_key])
+        for ids in smplRegion[_key]:
+            if ids not in footIdsLR[:,1]:
+                ic(_key,ids)
+    all_idsR = np.concatenate(all_idsR)
+    for idx in footIdsLR[:,1]:
+        if idx not in all_idsR:
+            ic(idx)
+
+    for _key in smplRegion.keys():
+        for i in range(smplRegion[_key].shape[0]):
+            Rids = smplRegion[_key][i]
+            a = np.where(footIdsLR[:,1]==Rids)[0]
+            smplRegion[_key][i] = footIdsLR[a,0]
+    for _key in insoleRegion.keys():
+        insoleRegion[_key][1,:] = 10-insoleRegion[_key][1,:]
+
+    RegionInsole2SMPLL = {
+        'insole':insoleRegion,
+        'smpl':smplRegion
+    }
+    ic(RegionInsole2SMPLL['smpl'])
+    ic(RegionInsole2SMPLL['insole'])
+    np.save('debug/insole2smpl/RegionInsole2SMPLL.npy', RegionInsole2SMPLL)
     exit()
+
+def visFootRegionMapping():
+    from lib.Utils.vis_foot import visFootImage
+    m_vis = visFootImage()
+    RegionInsole2SMPLR = np.load('debug/insole2smpl/RegionInsole2SMPLR.npy', allow_pickle=True).item()
+    insoleRegionR,smplRegionR = RegionInsole2SMPLR['insole'],RegionInsole2SMPLR['smpl']
+
+    RegionInsole2SMPLL = np.load('debug/insole2smpl/RegionInsole2SMPLL.npy', allow_pickle=True).item()
+    insoleRegionL,smplRegionL = RegionInsole2SMPLL['insole'],RegionInsole2SMPLL['smpl']
+
+    '''visual'''
+    insole_img = []
+    for insoleRegion in [insoleRegionL,insoleRegionR]:
+        color_map = np.ones([33,11,3])*255
+        color_map[insoleRegion['BigToe'][0,:],insoleRegion['BigToe'][1,:],:] = rgb_code['Red']
+        color_map[insoleRegion['MidToe'][0,:],insoleRegion['MidToe'][1,:],:] = rgb_code['Blue']
+        color_map[insoleRegion['SmallToe'][0,:],insoleRegion['SmallToe'][1,:],:] = rgb_code['Green']
+        color_map[insoleRegion['Front0'][0,:],insoleRegion['Front0'][1,:],:] = rgb_code['Purple']
+        color_map[insoleRegion['Front1'][0,:],insoleRegion['Front1'][1,:],:] = rgb_code['LightSlateGray']
+        color_map[insoleRegion['Front2'][0,:],insoleRegion['Front2'][1,:],:] = rgb_code['Cyan']
+        color_map[insoleRegion['Mid0'][0,:],insoleRegion['Mid0'][1,:],:] = rgb_code['OliveDrab']
+        color_map[insoleRegion['Mid1'][0,:],insoleRegion['Mid1'][1,:],:] = rgb_code['Orange']
+        color_map[insoleRegion['Back0'][0,:],insoleRegion['Back0'][1,:],:] = rgb_code['FireBrick']
+        color_map[insoleRegion['Back1'][0,:],insoleRegion['Back1'][1,:],:] = rgb_code['LightSeaGreen']
+        insole_img.append(color_map)
+    insole_img = np.concatenate(insole_img,axis=1)
+    cv2.imwrite('debug/insole2smpl/insoleRegion.png',insole_img.astype(np.uint8))
+
+    footIdsL, footIdsR = m_vis.footIdsL, m_vis.footIdsR
+    v_footL, v_footR = m_vis.v_footL, m_vis.v_footR
+    foot_regions = ['BigToe','MidToe','SmallToe','Front0','Front1','Front2','Mid0','Mid1','Back0','Back1']
+    colors = [rgb_code['Red'],rgb_code['Blue'],rgb_code['Green'],rgb_code['Purple'],
+              rgb_code['LightSlateGray'],rgb_code['Cyan'],rgb_code['OliveDrab'],
+              rgb_code['Orange'],rgb_code['FireBrick'],rgb_code['LightSeaGreen']]
+
+    smpl_img = []
+    for footIds,v_foot,smplRegion in zip([footIdsL, footIdsR],[v_footL, v_footR],[smplRegionL,smplRegionR]):
+        vert_color = np.ones([footIds.shape[0], 3])
+        for region_name, color_name in zip(foot_regions,colors):
+            ic(region_name, color_name)
+            smpl_ids = smplRegion[region_name]
+            for ids in smpl_ids:
+                b = np.where(footIds==ids)[0]
+                vert_color[b] = color_name
+
+        imgL = m_vis.drawSMPLFoot(v_foot, footIds,vert_color=vert_color)
+        smpl_img.append(imgL)
+    smpl_img = np.concatenate(smpl_img,axis=1)
+    cv2.imwrite('debug/insole2smpl/smpl_foot.png', smpl_img.astype(np.uint8))
+    exit()
+
 
 if __name__ == '__main__':
-    footRegion()
-    # mapInsole2Smpl()
+    # footRegionMapping()
+    # footRegionMappingRight2Left()
+    visFootRegionMapping()
     save_dir = 'debug/insole2smpl'
     insoleMaskR = np.loadtxt('E:/dataset/PressureDataset/insole_mask/insoleMaskR.txt').astype(np.int32)
     footIdsL = np.loadtxt('essentials/footL_ids.txt').astype(np.int32)
@@ -204,12 +300,12 @@ if __name__ == '__main__':
         vert_colorR = getVertsColor(pressure_img[:,11:,:], footIdsR, insole2smplR)
         imgR = drawSMPLFoot(v_footR,footIdsR,np.array(model_temp.faces),
                             vert_color=vert_colorR,save_name=None,
-                            point_size=40)
+                            point_size=4)
 
         vert_colorL = getVertsColor(pressure_img[:,:11,:], footIdsL, insole2smplL)
         imgL = drawSMPLFoot(v_footL,footIdsL,np.array(model_temp.faces),
                             vert_color=vert_colorL,save_name=None,
-                            point_size=40)
+                            point_size=4)
         img = np.concatenate([cv2.resize(pressure_img, (2200, 3350)),imgL,imgR],axis=1)
         cv2.imwrite('debug/insole2smpl/results/%03d.png'%insole_ids,img)
 
--- lib/Utils/labelSMPLPressure.py
+++ lib/Utils/labelSMPLPressure.py
@@ -14,26 +14,22 @@ from vis_foot import visFootImage
 from fileio import saveImgSeqAsvideo
 
 class SMPLFootLabel():
-    def __init__(self,basedir,sub_ids,seq_name):
+    def __init__(self,basedir):
         self.basedir = basedir
-        self.sub_ids = sub_ids
-        self.seq_name = seq_name
+
+        self.m_fimg = visFootImage()
 
         self.footIdsL = np.loadtxt('essentials/footL_ids.txt').astype(np.int32)
         self.footIdsR = np.loadtxt('essentials/footR_ids.txt').astype(np.int32)
         self.footLR = np.loadtxt('essentials/footLR_ids.txt').astype(np.int32)
         self.insole2smplR = np.load('essentials/pressure/insole2smplR.npy', allow_pickle=True).item()
         self.insole2smplL = np.load('essentials/pressure/insole2smplL.npy', allow_pickle=True).item()
+        self.RegionInsole2SMPLL = np.load('essentials/pressure/RegionInsole2SMPLL.npy', allow_pickle=True).item()
+        self.RegionInsole2SMPLR = np.load('essentials/pressure/RegionInsole2SMPLR.npy', allow_pickle=True).item()
 
-        self.m_fimg = visFootImage()
+        if self.footIdsL.shape[0] == self.footIdsR.shape[0]:
+            self.footVertsNum = self.footIdsL.shape[0]
 
-        insole_name = insole_sync[self.sub_ids][self.seq_name]
-        insole_path = osp.join(self.basedir, 'insole_pkl', insole_name + '.pkl')
-        with open(insole_path, "rb") as f:
-            insole_data = pickle.load(f)
-        indice_name = osp.join(self.basedir, 'Synced_indice', insole_name + '*')
-        synced_indice = np.loadtxt(glob.glob(indice_name)[0]).astype(np.int32)
-        self.insole_data = insole_data[synced_indice]
 
     def getVertsPress(self, press_data,footIds,insole2smpl):
         smpl_press = np.zeros([footIds.shape[0]],dtype=np.float32)
@@ -57,10 +53,10 @@ class SMPLFootLabel():
         imgColor = cv2.applyColorMap(img, cv2.COLORMAP_HOT)
         return imgColor
 
-    def visVertslabel(self,img_H=512,save_dir=None):
-        for frame_ids in trange(self.insole_data.shape[0]):
-            img = cv2.imread(osp.join(self.basedir,'RGBD', self.sub_ids,self.seq_name,'color/%03d.png'%frame_ids))
-            press_data = self.insole_data[frame_ids]
+    def visVertslabel(self,insole_data,sub_ids,seq_name,img_H=512,save_dir=None):
+        for frame_ids in trange(insole_data.shape[0]):
+            img = cv2.imread(osp.join(self.basedir,'RGBD', sub_ids,seq_name,'color/%03d.png'%frame_ids))
+            press_data = insole_data[frame_ids]
             press_img = self.show_insole(press_data)
             smpl_pressL = self.getVertsPress(press_data[0],self.footIdsL,self.insole2smplL)
             smpl_pressR = self.getVertsPress(press_data[1],self.footIdsR,self.insole2smplR)
@@ -77,10 +73,83 @@ class SMPLFootLabel():
             save_img = np.concatenate([img,press_img,smpl_img,cont_img],axis=1)
             cv2.imwrite(osp.join(save_dir,'%03d.png'%frame_ids),save_img)
 
+    def catRegion(self,is_cat=True):
+        if is_cat:
+            insoleRegionL, smplRegionL = self.RegionInsole2SMPLL['insole'], self.RegionInsole2SMPLL['smpl']
+            insoleRegionR, smplRegionR = self.RegionInsole2SMPLR['insole'], self.RegionInsole2SMPLR['smpl']
+            ic(insoleRegionL.keys())
+            _insoleRegionL={}
+            _insoleRegionL['Toe'] = np.concatenate([insoleRegionL['BigToe'],insoleRegionL['MidToe'],
+                                                    insoleRegionL['SmallToe']], axis=1)
+            _insoleRegionL['Front'] = np.concatenate([insoleRegionL['Front0'],insoleRegionL['Front1'],
+                                                    insoleRegionL['Front2']], axis=1)
+            _insoleRegionL['Mid'] = np.concatenate([insoleRegionL['Mid0'],insoleRegionL['Mid1']], axis=1)
+            _insoleRegionL['Back'] = np.concatenate([insoleRegionL['Back0'],insoleRegionL['Back1']], axis=1)
+
+            _smplRegionL = {}
+            _smplRegionL['Toe'] = np.array(list(set(smplRegionL['BigToe'].tolist() +
+                                smplRegionL['MidToe'].tolist() + smplRegionL['SmallToe'].tolist())))
+            _smplRegionL['Front'] = np.array(list(set(smplRegionL['Front0'].tolist() +
+                                smplRegionL['Front1'].tolist() + smplRegionL['Front2'].tolist())))
+            _smplRegionL['Mid'] = np.array(list(set(smplRegionL['Mid0'].tolist() + smplRegionL['Mid1'].tolist())))
+            _smplRegionL['Back'] = np.array(list(set(smplRegionL['Back0'].tolist() + smplRegionL['Back1'].tolist())))
+
+            _insoleRegionR={}
+            _insoleRegionR['Toe'] = np.concatenate([insoleRegionR['BigToe'],insoleRegionR['MidToe'],
+                                                    insoleRegionR['SmallToe']], axis=1)
+            _insoleRegionR['Front'] = np.concatenate([insoleRegionR['Front0'],insoleRegionR['Front1'],
+                                                    insoleRegionR['Front2']], axis=1)
+            _insoleRegionR['Mid'] = np.concatenate([insoleRegionR['Mid0'],insoleRegionR['Mid1']], axis=1)
+            _insoleRegionR['Back'] = np.concatenate([insoleRegionR['Back0'],insoleRegionR['Back1']], axis=1)
+
+            _smplRegionR = {}
+            _smplRegionR['Toe'] = np.array(list(set(smplRegionR['BigToe'].tolist() +
+                                smplRegionR['MidToe'].tolist() + smplRegionR['SmallToe'].tolist())))
+            _smplRegionR['Front'] = np.array(list(set(smplRegionR['Front0'].tolist() +
+                                smplRegionR['Front1'].tolist() + smplRegionR['Front2'].tolist())))
+            _smplRegionR['Mid'] = np.array(list(set(smplRegionR['Mid0'].tolist() + smplRegionR['Mid1'].tolist())))
+            _smplRegionR['Back'] = np.array(list(set(smplRegionR['Back0'].tolist() + smplRegionR['Back1'].tolist())))
+        else:
+            _insoleRegionL, _smplRegionL = self.RegionInsole2SMPLL['insole'],self.RegionInsole2SMPLL['smpl']
+            _insoleRegionR, _smplRegionR = self.RegionInsole2SMPLR['insole'],self.RegionInsole2SMPLR['smpl']
+        return _insoleRegionL, _smplRegionL, _insoleRegionR, _smplRegionR
+
+    def press2contact(self,press_data,footIds,insoleRegion,smplRegion,th=50.):
+        _label = np.zeros(6890)
+        for keyl in insoleRegion.keys():
+            insolel = insoleRegion[keyl]
+            sum_press = np.sum(press_data[insolel[0, :], insolel[1, :]])
+            if sum_press>th:
+                _label[smplRegion[keyl]] = 1
+        _label = _label[footIds]
+        return _label
+
+    def labelContact(self,sub_ids,seq_name):
+        insoleRegionL, smplRegionL, insoleRegionR, smplRegionR = self.catRegion(is_cat=True)
+
+        '''load insole data'''
+        insole_name = insole_sync[sub_ids][seq_name]
+        insole_path = osp.join(self.basedir, 'insole_pkl', insole_name + '.pkl')
+        with open(insole_path, "rb") as f:
+            insole_data = pickle.load(f)
+        indice_name = osp.join(self.basedir, 'Synced_indice', insole_name + '*')
+        synced_indice = np.loadtxt(glob.glob(indice_name)[0]).astype(np.int32)
+        insole_data = insole_data[synced_indice]
+
+        contact_label = []
+        for frame_ids in trange(insole_data.shape[0]):
+            press_data = insole_data[frame_ids]
+            left_label = self.press2contact(press_data[0,...], self.footIdsL, insoleRegionL,smplRegionL)
+            right_label = self.press2contact(press_data[1,...], self.footIdsR, insoleRegionR,smplRegionR)
+            cont_label = np.stack([left_label,right_label])
+            contact_label.append(cont_label)
+        contact_label = np.stack(contact_label)
+        np.save(osp.join(self.basedir,'RGBD',sub_ids,seq_name,'contact_label.npy'),contact_label)
+
+
 
 if __name__ == "__main__":
-    m_sfl = SMPLFootLabel(
-        basedir='E:/dataset/PressureDataset/20230422',
+    m_sfl = SMPLFootLabel(basedir='E:/dataset/PressureDataset/20230422')
+    m_sfl.labelContact(
         sub_ids='S12',
-        seq_name = 'MoCap_20230422_145422')
-    m_sfl.visVertslabel(save_dir='debug/insole2smpl/pressure')
+        seq_name='MoCap_20230422_145422')
